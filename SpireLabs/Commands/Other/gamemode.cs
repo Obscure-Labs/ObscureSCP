a215f8057f7c7eea35a349454a5edaf4e7601e59
ImKevinTbh <kevinj.Watson@outlook.com>
2023-11-21 04:47:46 +0000

    Gamemodes work begins
    This works for the most part, needs tweaking to randomise players on each team but should be good for now

---
+++ SpireLabs/Commands/Other/gamemode.cs
@@ -0,0 +1,38 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.Permissions.Extensions;
+using MEC;
+using SpireLabs.Gamemode_Handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpireLabs.Commands.Other
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class JailbirdTDM : ICommand
+    {
+        public JailbirdTDM() => LoadGeneratedCommands();
+        public string Command { get; set; } = "gamemode";
+        public string[] Aliases { get; set; } = new string[] { };
+        public string Description { get; set; } = "Testing only!";
+        public void LoadGeneratedCommands() { }
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+
+            if (((CommandSender)sender).CheckPermission("*"))
+            {
+                Timing.RunCoroutine(jailBirdTDM.runJbTDM());
+                Round.Start();
+                response = "Force Starting Mode";
+                return false;
+            }
+            else response = "lol no";
+
+            return false;
+        }
+    }
+}
