344109c07acba1a479248eda8a3107888aea7cc0
ImKevinTbh <kevinj.Watson@outlook.com>
2023-11-22 01:57:17 +0000

    Minigames update
    Support random map picking, random loadouts improved

--- SpireLabs/Gamemode Handler/jailBirdTDM.cs
+++ SpireLabs/Gamemode Handler/jailBirdTDM.cs
@@ -18,11 +18,15 @@ using Exiled.API.Enums;
 using CustomItems.API;
 using SpireLabs.Items;
 using InventorySystem.Items;
+using Exiled.API.Features.Items;
 
 namespace SpireLabs.Gamemode_Handler
 {
     internal static class jailBirdTDM
     {
+
+        static string mapName = "empty";
+        public static string[] maps = { "pvpA1_2t", "pvpA2_2t", "pvpRA1_2t" };
         static bool gamemodeactive = false;
         static bool team = true;
         public static IEnumerator<float> startJbTDM()
@@ -82,18 +86,33 @@ namespace SpireLabs.Gamemode_Handler
             }
 
         public static IEnumerator<float> runJbTDM()
-            {
-                gamemodeactive = true;
+        {
+            Vector3 spawnCI = new Vector3(0f, 1000f, 0f);
+            Vector3 spawnNTF = new Vector3(0f, 1000f, 0f);
+            var rndMap = new System.Random();
+            int numMap = rndMap.Next(0, 4);
+            var rnd69 = new System.Random();
+            int num69 = rnd69.Next(0, 4);
+            switch (rndMap.Next(0, maps.Count()))
+                {
+                    case 0: mapName = maps[0]; spawnCI = new Vector3(8.48f, 1106.5f, 30.46f); spawnNTF = new Vector3(-20.8f, 1107.5f, 51.66f); break;
+                    case 1: mapName = maps[1]; spawnCI = new Vector3(9.7f, 1102f, 52.46f); spawnNTF = new Vector3(-20.20f, 1102f, 35.37f); break;
+                    case 2:
+                        mapName = maps[2]; 
+                        spawnCI = new Vector3(9.7f, 1102f, 52.46f); 
+                        spawnNTF = new Vector3(-53.97f, 1107f, 58.32f);
+                        num69 = rnd69.Next(0, 3); // This is to prevent balls and grenades on this map due to it being smaller and easy for players to run out of items before anyone actually dies
+                        break;
+            }
+
+            gamemodeactive = true;
                 Log.Warn("Unloading all default maps");
                 MapEditorReborn.API.Features.MapUtils.LoadMap("empty");
                 Log.Warn("Loading Map: pvpA1_2t");
-                MapEditorReborn.API.Features.MapUtils.LoadMap("pvpA1_2t");
+                MapEditorReborn.API.Features.MapUtils.LoadMap($"{mapName}");
                 Log.Warn("Starting checks for players with wrong roles");
                 Timing.WaitForSeconds(0.6f);
                 Timing.RunCoroutine(lateJoin());
-                Server.Broadcast.BroadcastMessage("MINIGAME ROUND!");
-                var rnd69 = new System.Random();
-                int num69 = rnd69.Next(0, 3);
 
             foreach (Player p in Player.List)
                 {
@@ -106,10 +125,13 @@ namespace SpireLabs.Gamemode_Handler
                     team = false;
                     p.Role.Set(RoleTypeId.ChaosConscript);
                     p.ClearInventory(true);
-                    p.Teleport(new UnityEngine.Vector3(8.48f, 1106.5f, 30.46f));
-                    p.EnableEffect(EffectType.Invigorated, 10, false);
+                    p.Teleport(spawnCI);
+                    p.EnableEffect(EffectType.RainbowTaste, 999, false);
                     p.EnableEffect(EffectType.Ensnared, 10, false);
                     p.EnableEffect(EffectType.Flashed, 10, false);
+                    p.EnableEffect(EffectType.SoundtrackMute, 999, false);
+                    p.EnableEffect(EffectType.DamageReduction, 10, false);
+                    p.ChangeEffectIntensity(EffectType.DamageReduction, 255, 1f);
                 }
                 else
                 {
@@ -117,35 +139,46 @@ namespace SpireLabs.Gamemode_Handler
                     team = true;
                     p.Role.Set(RoleTypeId.NtfSergeant);
                     p.ClearInventory(true);
-                    p.Teleport(new UnityEngine.Vector3(-20.8f, 1107.5f, 51.66f));
-                    p.EnableEffect(EffectType.Invigorated, 10, false);
+                    p.Teleport(spawnNTF);
+                    p.EnableEffect(EffectType.RainbowTaste, 999, false);
                     p.EnableEffect(EffectType.Ensnared, 10, false);
                     p.EnableEffect(EffectType.Flashed, 10, false);
+                    p.EnableEffect(EffectType.SoundtrackMute, 999, false);
+                    p.EnableEffect(EffectType.DamageReduction, 10, false);
+                    p.ChangeEffectIntensity(EffectType.DamageReduction, 255, 1f);
                 }
-                p.Broadcast(5, "<color=green><b>MINIGAME ROUND!");
+                p.Broadcast(5, "<color=green><b>MINIGAME ROUND BEGINS IN 10S!");
                 if (num69 == 0)
                 {
-                    Log.Warn("Giving 0");
+                    Log.Warn("Giving Snipers");
                     Exiled.CustomItems.API.Features.CustomItem.Get((uint)1).Give(p);
                     p.AddAmmo(AmmoType.Ammo44Cal, 99);
                 }
                 if (num69 == 1)
                 {
-                    Log.Warn("Giving 1");
+                    Log.Warn("Giving Jailbirds");
                     var item = p.AddItem(ItemType.Jailbird);
                     p.CurrentItem = item;
                 }
                 if (num69 == 2)
                 {
-                    Log.Warn("Giving 2");
+                    Log.Warn("Giving Particle Disrupters");
+                    Item item = p.AddItem(ItemType.ParticleDisruptor);
+                    p.AddItem(ItemType.ParticleDisruptor);
+                    p.CurrentItem = item;
+                }
+                if (num69 == 3)
+                {
+                    Log.Warn("Giving Grenades and Balls");
                     var item = p.AddItem(ItemType.SCP018);
+                    p.AddItem(ItemType.GrenadeHE);
+                    p.AddItem(ItemType.GrenadeHE);
                     p.AddItem(ItemType.SCP018);
+                    p.AddItem(ItemType.GrenadeHE);
+                    p.AddItem(ItemType.GrenadeHE);
                     p.AddItem(ItemType.SCP018);
-                    p.AddItem(ItemType.SCP018);
-                    p.AddItem(ItemType.SCP018);
-                    p.AddItem(ItemType.SCP018);
-                    p.AddItem(ItemType.SCP018);
-                    p.AddItem(ItemType.SCP018);
+                    p.AddItem(ItemType.GrenadeHE);
+                    p.EnableEffect(EffectType.Scp207, 999, false);
                     p.CurrentItem = item;
                 }
                 p.Scale = new Vector3(1, 1, 1);
