225379b4e6f1f7327780b481048f8aa0f96e49e4
I'mIsaac <isaac@asterias.co.uk>
2023-12-24 17:29:31 +0000

    More custom items
    grenade launcher
    cluster grenade
    impact grenade

---
+++ SpireLabs/Items/aidsLauncher.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Pickups;
+using Exiled.API.Features.Spawn;
+using Exiled.CustomItems.API.Features;
+using Exiled.Events.EventArgs.Player;
+using MEC;
+using UnityEngine;
+using Player = Exiled.Events.Handlers.Player;
+using SpireSCP.GUI.API.Features;
+using CustomItems.API;
+using Exiled.API.Features.Pickups.Projectiles;
+using InventorySystem.Items.ThrowableProjectiles;
+using Exiled.Events.EventArgs.Map;
+using System.Linq;
+using InventorySystem.Items.Firearms.BasicMessages;
+using InventorySystem.Items.Firearms.Attachments.Components;
+using Exiled.API.Structs;
+using Utf8Json.Resolvers.Internal;
+using JetBrains.Annotations;
+using System.Reflection;
+using Exiled.API.Features.Components;
+
+namespace SpireLabs.Items
+{
+    [CustomItem(ItemType.GunLogicer)]
+    public class aidsLauncher : Exiled.CustomItems.API.Features.CustomWeapon
+    {
+
+
+        private Exiled.CustomItems.API.Features.CustomGrenade? loadedCustomGrenade = null;
+        private ProjectileType loadedGrenade = ProjectileType.FragGrenade;
+        public override float Damage { get; set; } = 0f;
+        public override string Name { get; set; } = "Nade Launcher";
+        public override uint Id { get; set; } = 3;
+        public override string Description { get; set; } = "\t";
+        public override float Weight { get; set; } = 1.25f;
+        public override byte ClipSize { get; set; } = 1;
+        public override SpawnProperties SpawnProperties { get; set; } = new()
+        {
+            Limit = 2,
+            DynamicSpawnPoints = new List<DynamicSpawnPoint>
+            {
+                new()
+                {
+                    Chance = 50,
+                    Location = Exiled.API.Enums.SpawnLocationType.InsideLocker,
+                },
+                new()
+                {
+                    Chance = 50,
+                    Location = Exiled.API.Enums.SpawnLocationType.InsideHczArmory,
+                }
+            },
+        };
+
+        protected override void SubscribeEvents()
+        {
+            Player.ChangedItem += Equipped;
+            base.SubscribeEvents();
+        }
+
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+        }
+
+        private void Equipped(ChangedItemEventArgs ev)
+        {
+            if (!Check(ev.Item)) return;
+            Manager.SendHint(ev.Player, "You equipped the <b>Grenade Launcher</b> \n <b>This will explode into multiple smaller grenades</b>.", 3.0f);
+        }
+
+        protected override void OnShooting(ShootingEventArgs ev)
+        {
+            ev.IsAllowed = false;
+
+            if (ev.Player.CurrentItem is Firearm firearm)
+                if (firearm.Ammo != ClipSize) ev.Player.Connection.Send(new RequestMessage(ev.Firearm.Serial, RequestType.Dryfire)); else firearm.Ammo -= 1;
+            Projectile projectile;
+            if(loadedCustomGrenade != null)
+            {
+                loadedCustomGrenade.Throw(ev.Player.Transform.position, 10f, 5f, 3f, ItemType.GrenadeHE, ev.Player);
+            }
+            else
+            {
+
+                switch (loadedGrenade)
+                {
+                    case ProjectileType.Scp018:
+                        projectile = ev.Player.ThrowGrenade(ProjectileType.Scp018).Projectile;
+                        break;
+                    case ProjectileType.Flashbang:
+                        projectile = ev.Player.ThrowGrenade(ProjectileType.Flashbang).Projectile;
+                        break;
+                    default:
+                        projectile = ev.Player.ThrowGrenade(ProjectileType.FragGrenade).Projectile;
+                        break;
+                }
+                projectile.GameObject.AddComponent<CollisionHandler>().Init(ev.Player.GameObject, projectile.Base);
+            }
+        }
+
+        protected override void OnReloading(ReloadingWeaponEventArgs ev)
+        {
+            ev.IsAllowed = false;
+            if (!(ev.Player.CurrentItem is Firearm firearm) || firearm.Ammo >= ClipSize) return;
+            foreach (Item item in ev.Player.Items.ToList())
+            {
+                if (item.Type != ItemType.GrenadeHE && item.Type != ItemType.GrenadeFlash) continue;
+                if(TryGet(item, out Exiled.CustomItems.API.Features.CustomItem? customNade))
+                {
+                    continue;
+                }
+
+                ev.Player.Connection.Send(new RequestMessage(ev.Firearm.Serial, RequestType.Reload));
+
+                Timing.CallDelayed(1.5f, () => firearm.Ammo = ClipSize);
+
+                loadedGrenade = item.Type == ItemType.GrenadeFlash ? ProjectileType.Flashbang :
+                    item.Type == ItemType.GrenadeHE ? ProjectileType.FragGrenade : ProjectileType.FragGrenade;
+                ev.Player.RemoveItem(item);
+                return;
+            }
+        }
+
+    } 
+}
---
+++ SpireLabs/Items/impactNades.cs
@@ -0,0 +1,59 @@
+﻿using CustomItems.API;
+using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Components;
+using Exiled.API.Features.Pickups.Projectiles;
+using Exiled.API.Features.Spawn;
+using Exiled.Events.EventArgs.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HarmonyLib.Code;
+
+namespace SpireLabs.Items
+{
+    [CustomItem(ItemType.GrenadeHE)]
+    public class impactNades : Exiled.CustomItems.API.Features.CustomGrenade
+    {
+        public override string Name { get; set; } = "Impact Grenade";
+        public override uint Id { get; set; } = 4;
+        public override string Description { get; set; } = "\t";
+        public override float Weight { get; set; } = 0.01f;
+        public override SpawnProperties SpawnProperties { get; set; } = new()
+        {
+            Limit = 2,
+            DynamicSpawnPoints = new List<DynamicSpawnPoint>
+            {
+                new()
+                {
+                    Chance = 50,
+                    Location = Exiled.API.Enums.SpawnLocationType.InsideLocker,
+                },
+                new()
+                {
+                    Chance = 50,
+                    Location = Exiled.API.Enums.SpawnLocationType.InsideHczArmory,
+                }
+            },
+        };
+        public override bool ExplodeOnCollision { get; set; } = false;
+        public override float FuseTime { get; set; } = 50f;
+
+        protected override void SubscribeEvents()
+        {
+            base.SubscribeEvents();
+        }
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+
+        }
+
+        protected override void OnThrownProjectile(ThrownProjectileEventArgs ev)
+        {
+            base.OnThrownProjectile(ev);
+            ev.Projectile.GameObject.AddComponent<CollisionHandler>().Init(ev.Player.GameObject, ev.Projectile.Base);
+        }
+    }
+}
