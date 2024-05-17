8e03ad1defcd065c660d4ff3aa6b4c4d9d935ffb
ImKevintbh <kevinj.Watson@outlook.com>
2024-05-17 03:48:04 +0100

    funny meme glow

--- SpireLabs/Items/Primitive.cs
+++ SpireLabs/Items/Primitive.cs
@@ -40,7 +40,7 @@ namespace ObscureLabs.Items
     public class Primitive : ICommand
     {
         public Primitive() => LoadGeneratedCommands();
-        public string Command { get; set; } = "primtive";
+        public string Command { get; set; } = "glow";
         public string[] Aliases { get; set; } = new string[] { };
         public string Description { get; set; } = "Cube camera";
         public void LoadGeneratedCommands() { }
@@ -50,29 +50,32 @@ namespace ObscureLabs.Items
         {
             Player pl = Player.Get((CommandSender)sender);
             Vector3 loc = pl.Transform.position;
-            Exiled.API.Features.Toys.Primitive p = Exiled.API.Features.Toys.Primitive.Create(new Vector3(loc.x, loc.y + 1.35f, loc.z), new Vector3(0, 0, 0), new Vector3(1f, 0.01f, 1f), false);
-            Exiled.API.Features.Toys.Primitive p1 = Exiled.API.Features.Toys.Primitive.Create(new Vector3(loc.x, loc.y + 1.35f, loc.z), new Vector3(45, 45, 45), new Vector3(1f, 0.01f, 1f), false);
+            //Exiled.API.Features.Toys.Primitive p = Exiled.API.Features.Toys.Primitive.Create(new Vector3(loc.x, loc.y + 1.35f, loc.z), new Vector3(0, 0, 0), new Vector3(1f, 0.01f, 1f), false);
+            //Exiled.API.Features.Toys.Primitive p1 = Exiled.API.Features.Toys.Primitive.Create(new Vector3(loc.x, loc.y + 1.35f, loc.z), new Vector3(45, 45, 45), new Vector3(1f, 0.01f, 1f), false);
 
 
-            p1.Collidable = false;
-            p1.Type = PrimitiveType.Cube;
-            p1.Color = new Color(50f, 0f, 50f, 0.45f);
-            p1.MovementSmoothing = 55;
-            p1.Scale = new Vector3(0.2f, 0.2f, 0.2f);
+            //p1.Collidable = false;
+            //p1.Type = PrimitiveType.Cube;
+            //p1.Color = new Color(50f, 0f, 50f, 0.45f);
+            //p1.MovementSmoothing = 55;
+            //p1.Scale = new Vector3(0.2f, 0.2f, 0.2f);
 
 
-            p.Collidable = false;
-            p.Type = PrimitiveType.Cube;
-            p.Color = new Color(50f, 25f, 50f, 0.005f);
-            p.MovementSmoothing = 55;
-            p.Scale = new Vector3(0.2f, 0.2f, 0.2f);
+            //p.Collidable = false;
+            //p.Type = PrimitiveType.Cube;
+            //p.Color = new Color(50f, 25f, 50f, 0.005f);
+            //p.MovementSmoothing = 55;
+            //p.Scale = new Vector3(0.2f, 0.2f, 0.2f);
 
+            // p1.Spawn();
 
-            p1.Spawn();
+            Exiled.API.Features.Toys.Light p = Exiled.API.Features.Toys.Light.Create(new Vector3(loc.x, loc.y + 1.35f, loc.z), new Vector3(0, 0, 0), new Vector3(1, 1, 1), true, Color.magenta);
+
             p.Spawn();
-
+            p.Range = 25;
+            p.Intensity = 15f;
             p.Base.gameObject.transform.SetParent(pl.GameObject.transform);
-            p1.Base.gameObject.transform.SetParent(pl.GameObject.transform);
+            //p1.Base.gameObject.transform.SetParent(pl.GameObject.transform);
 
             if (Plugin.OBJLST.FirstOrDefault(x => x.Player.NetworkIdentity == pl.NetworkIdentity) == null)
                 {
@@ -93,7 +96,7 @@ namespace ObscureLabs.Items
     public class UnPrimitive : ICommand
     {
         public UnPrimitive() => LoadGeneratedCommands();
-        public string Command { get; set; } = "unprimitive";
+        public string Command { get; set; } = "unglow";
         public string[] Aliases { get; set; } = new string[] { };
         public string Description { get; set; } = "Cube";
         public void LoadGeneratedCommands() { }
