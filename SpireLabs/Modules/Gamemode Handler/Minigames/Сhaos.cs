187473a523009f60512fd8730827df8e54a4f229
ImKevinTbh <kevinj.Watson@outlook.com>
2024-08-22 14:32:12 +0100

    Chaos Mode Randomiser Rework
    Womp womp

--- SpireLabs/Modules/Gamemode Handler/Minigames/Сhaos.cs
+++ SpireLabs/Modules/Gamemode Handler/Minigames/Сhaos.cs
@@ -9,7 +9,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Log = Exiled.API.Features.Log;
-
+using UnityEngine;
+using Exiled.API.Features.Pickups;
+using Exiled.API.Extensions;
+using SpireSCP.GUI.API.Features;
+using CustomItem = Exiled.CustomItems.API.Features.CustomItem;
 namespace ObscureLabs.Modules.Gamemode_Handler.Minigames
 {
     public class Chaos : Module
@@ -21,31 +25,94 @@ namespace ObscureLabs.Modules.Gamemode_Handler.Minigames
         public override bool Enable()
         {
             Exiled.Events.Handlers.Server.RespawningTeam += OnRespawningTeam;
-
+            Timing.RunCoroutine(GunSpawnCoroutine());
+            Server.FriendlyFire = true;
             return base.Enable();
+
         }
 
         public override bool Disable()
         {
             Exiled.Events.Handlers.Server.RespawningTeam -= OnRespawningTeam;
 
+            Server.FriendlyFire = false;
             return base.Disable();
         }
 
+        private static List<CustomItem> _customitemlist = new()
+        {
+            CustomItem.Get((uint)2), // ClusterHE
+            CustomItem.Get((uint)7), // ClusterFlash
+            CustomItem.Get((uint)0), // EssentialOils
+            CustomItem.Get((uint)4), // NovaGrenade
+            CustomItem.Get((uint)1), // sniper
+            CustomItem.Get((uint)3), // grenade launcher
+            CustomItem.Get((uint)5), // ER16
+            CustomItem.Get((uint)6), // Particle Collapser
+        };
+
+
+
+        public static IEnumerator<float> GunSpawnCoroutine()
+        {
+            Log.Info("Running custom item spawner for vanilla round");
+            foreach (Room room in Room.List.ToList())
+            {
+                yield return Timing.WaitForSeconds(0.1f);
+
+                foreach (Pickup pickup in room.Pickups.ToList())
+                {
+                    if (pickup == null)
+                    {
+                        continue;
+                    }
+                    var customItemChance = UnityEngine.Random.Range(0, 101);
+                    var isCustomItem = customItemChance >= 50 && customItemChance <= 55;
+
+                    yield return Timing.WaitForSeconds(0.1f);
+
+                    if (isCustomItem)
+                    {
+                        try
+                        {
+                            _customitemlist.RandomItem().Spawn(pickup.Position);
+
+                            Log.Info($"CHAOS: Made new CUSTOM item in {room.Type}");
+
+                            pickup.Destroy();
+                            Log.Warn($"CHAOS: Removed original item in {room.Type}");
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Warn(e);
+
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            var randomItemType = Enum.GetValues(typeof(ItemType)).ToArray<ItemType>().GetRandomValue();
+
+                            Pickup.CreateAndSpawn(randomItemType, pickup.Position, pickup.Rotation);
+                            Log.Info($"CHAOS: Made new item in {room.Type}");
+
+                            pickup.Destroy();
+                            Log.Warn($"CHAOS: Removed original item in {room.Type}");
+                        }
+                        catch (Exception e) 
+                        {
+                            Log.Warn(e);
+                        }
+
+                    }
+                }
+            }
+        }
+
+
         public static void OnRespawningTeam(RespawningTeamEventArgs ev)
         {
-            if (Plugin.IsActiveEventround && Plugin.EventRoundType is not API.Enums.EventRoundType.Chaos)
-            {
-                return;
-            }
-
-            if (!Plugin.IsActiveEventround)
-            {
-                return;
-            }
-
-            else
-            {
                 Log.Info(Plugin.EventRoundType);
                 ev.IsAllowed = false;
                 Log.Info("Run ChaosMode Spawn wave");
@@ -59,8 +126,8 @@ namespace ObscureLabs.Modules.Gamemode_Handler.Minigames
                         player.AddItem(ItemType.Coin, 2);
                         player.AddItem(ItemType.KeycardMTFCaptain, 1);
                         player.AddItem(ItemType.Flashlight, 1);
-                        player.Teleport(RoomType.EzDownstairsPcs);
-                        Respawn.NtfTickets = 100;
+                    RandomSpawn(player);
+                    Respawn.NtfTickets = 100;
                         Respawn.TimeUntilNextPhase = 45;
                     }
                 }
@@ -73,12 +140,50 @@ namespace ObscureLabs.Modules.Gamemode_Handler.Minigames
                         player.AddItem(ItemType.Coin, 2);
                         player.AddItem(ItemType.KeycardMTFCaptain, 1);
                         player.AddItem(ItemType.Flashlight, 1);
-                        player.Teleport(RoomType.EzDownstairsPcs);
-                        Respawn.ChaosTickets = 100;
+                    RandomSpawn(player);
+                    Respawn.ChaosTickets = 100;
                         Respawn.TimeUntilNextPhase = 45;
                     }
                 }
             }
+
+        public static void RandomSpawn(Player player)
+        {
+            var number = UnityEngine.Random.Range(0, 2);
+            var goodRoom = false;
+
+            //what the magic number
+            var room = Room.List.ElementAt(4);
+            var door = Room.List.ElementAt(4).Doors.FirstOrDefault();
+
+            while (!goodRoom)
+            {
+                var roomNd = new System.Random();
+                var roomNum = roomNd.Next(0, Room.List.Count());
+
+                var room2 = Room.List.ElementAt(roomNum);
+
+                if (Map.IsLczDecontaminated)
+                {
+                    if (room2.Type is not RoomType.HczTesla or RoomType.HczElevatorA or RoomType.HczElevatorB && Room.List.ElementAt(roomNum).Zone is not ZoneType.LightContainment)
+                    {
+                        goodRoom = true;
+                        door = Room.List.ElementAt(roomNum).Doors.FirstOrDefault();
+                    }
+                }
+                else
+                {
+                    if (room2.Type != RoomType.HczTesla)
+                    {
+                        goodRoom = true;
+
+                        door = Room.List.ElementAt(roomNum).Doors.FirstOrDefault() ;
+                    }
+                }
+
+            }
+
+            player.Teleport(new Vector3(door.Position.x, door.Position.y + 1.5f, door.Position.z));
         }
 
         public static IEnumerator<float> RunChaosCoroutine()
@@ -104,7 +209,7 @@ namespace ObscureLabs.Modules.Gamemode_Handler.Minigames
                     player.AddItem(ItemType.Coin, 2);
                     player.AddItem(ItemType.KeycardScientist, 1);
                     player.AddItem(ItemType.Flashlight, 1);
-                    player.Teleport(RoomType.LczClassDSpawn);
+                    RandomSpawn(player);
                 }
                 PList.Remove(player);
                 //Player.List.Remove(p);
@@ -119,7 +224,7 @@ namespace ObscureLabs.Modules.Gamemode_Handler.Minigames
                     player.AddItem(ItemType.Coin, 2);
                     player.AddItem(ItemType.KeycardScientist, 1);
                     player.AddItem(ItemType.Flashlight, 1);
-                    player.Teleport(RoomType.LczClassDSpawn);
+                    RandomSpawn(player);
                 }
             }
 
